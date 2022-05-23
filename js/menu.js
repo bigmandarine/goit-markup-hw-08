d7d4c56ff7af8101b44011716fd314f67cb7a118
Dima Kuzko <bigmandarine1991@gmail.com>
2022-05-23 14:25:37 +0300

    убрал прокрутку при моб меню

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@
   const mobileMenuRef = document.querySelector(' [data-menu]');
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    document.body.classList.toggle('is-open');
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('artia-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
