35fc31d90cc0de4d3ebd69cda622b103ea6883c2
Dima Kuzko <bigmandarine1991@gmail.com>
2022-06-08 18:24:00 +0300

    доработки

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,10 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector(' [data-menu]');
+  const body = document.querySelector('body');
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-    document.body.classList.toggle('is-open');
+    body.classList.toggle('is-open');
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('artia-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
